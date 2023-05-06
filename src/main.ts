c220b7d71b9c3d2d45e03fc0bec3e9e06a671a1d
GiovannyGarzon <giovankyandres@gmail.com>
2023-05-05 23:12:27 -0400

    updates

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+if(!navigator.geolocation){
+    alert('Tu navegador no soporta el geolocator')
+    throw new Error('Tu navegador no soporta el geolocator')
+}
+
 createApp(App).use(store).use(router).mount('#app')
